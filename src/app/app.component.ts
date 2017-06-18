bbba6f23352566b1630b303a25a6e7d45cc90968
Sakib <testnilavo2@gmail.com>
2017-06-18 16:41:15 +0600

    start

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, NgZone } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import firebase from 'firebase';
+
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any;
+  zone: NgZone;
+  
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+ 
+ var config = {
+    apiKey: "AIzaSyBXmZRlFuP4XO1oRWHrLrBNOoQXQy4abqY",
+    authDomain: "myevents-ddf66.firebaseapp.com",
+    databaseURL: "https://myevents-ddf66.firebaseio.com",
+    projectId: "myevents-ddf66",
+    storageBucket: "myevents-ddf66.appspot.com",
+    messagingSenderId: "1062080339674"
+  };
+  firebase.initializeApp(config);
+
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+      this.zone = new NgZone({});
+const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+  this.zone.run( () => {
+    if (!user) {
+      this.rootPage = 'login';
+      unsubscribe();
+    } else { 
+      this.rootPage = HomePage;
+      unsubscribe();
+    }
+  });     
+});
+
+
+    });
+  }
+}
+
